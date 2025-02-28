ab900bab156a0319681ed8c0018e26955f7b35e6
Tawney Kirkland <tawneykirkland@macbookpro.mynetworksettings.com>
2025-02-28 17:41:23 -0500

    Set up dbt project and add src table

---
+++ credit/creditproject/models/src/src_credit.sql
@@ -0,0 +1,18 @@
+WITH src_credit AS (
+    SELECT * FROM CREDIT.RAW.RAW_CREDIT
+)
+
+SELECT
+    SERIOUSDLQIN2YRS AS serious_delinquency,
+    REVOLVINGUTILIZATIONOFUNSECUREDLINES AS credit_usage_pct,
+    age,
+    NUMBEROFTIME30_59DAYSPASTDUENOTWORSE AS count_3059_days_past_due,
+    DEBTRATIO AS debt_ratio,
+    MONTHLYINCOME AS monthly_income,
+    NUMBEROFOPENCREDITLINESANDLOANS AS credit_loans_count,
+    NUMBEROFTIMES90DAYSLATE AS count_90_days_late,
+    NUMBERREALESTATELOANSORLINES AS real_estate_line_loans_count,
+    NUMBEROFTIME60_89DAYSPASTDUENOTWORSE AS count_6089_days_past_due,
+    NUMBEROFDEPENDENTS AS count_dependents
+FROM
+    src_credit
